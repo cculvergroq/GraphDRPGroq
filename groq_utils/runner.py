1646e0f85ab4f3419b43adf0559117d3a4aaf0b6
Chris Culver <cculver@groq.com>
2024-01-22 08:11:58 -0800

    Groq runner added - need to verify

--- groq_utils/runner.py
+++ groq_utils/runner.py
@@ -334,3 +334,127 @@ class OnnxRunner:
         )
 
         return test_scores
+    
+
+from groq.runner import tsp   
+
+class GroqRunner:
+    def __init__(self, params):
+        self.params=params
+        self.data_loader=self.load_infer_data()
+        self.model=self.load_model()
+    
+    def load_model(self):
+        return tsp.create_tsp_runner(self.params['iop_name'])
+    
+
+    def load_infer_data(self):
+        """ Load inference data
+        
+        Args:
+            params (dict): dict of CANDLE/IMPROVE parameters and parsed values.
+            
+        Returns:
+            data: data for inference predicting
+        """
+        
+        # ------------------------------------------------------
+        # [Req] Create output dir
+        # ------------------------------------------------------
+        frm.create_outdir(outdir=self.params["infer_outdir"])
+
+        # ------------------------------------------------------
+        # [Req] Create data names for test set
+        # ------------------------------------------------------
+        test_data_fname = frm.build_ml_data_name(self.params, stage="test")
+
+        # GraphDRP -- remove data_format
+        test_data_fname = test_data_fname.split(self.params["data_format"])[0]
+
+        # ------------------------------------------------------
+        # Prepare dataloaders to load model input data (ML data)
+        # ------------------------------------------------------
+        print("\nTest data:")
+        print(f"test_ml_data_dir: {self.params['test_ml_data_dir']}")
+        print(f"test_batch: {self.params['test_batch']}")
+        test_loader = build_GraphDRP_dataloader(self.params["test_ml_data_dir"],
+                                                test_data_fname,
+                                                self.params["test_batch"],
+                                                shuffle=False)
+        
+        return test_loader
+    
+    def run_predictions(self):
+        """ Run predictions
+        
+        """
+        # Compute predictions
+        total_labels=torch.Tensor()
+        total_preds=torch.Tensor()
+        ort_session = ort.InferenceSession(self.params['onnx_name'])
+        print("Make prediction for {} samples...".format(len(self.data_loader.dataset)))
+        with torch.no_grad():
+            for data in self.data_loader:
+            # for batch in self.data_loader:
+            #    for data in batch:
+            #       do stuff
+                
+                # data=data.to(self.device)
+                # output, _ = self.model(data.x, data.edge_index, data.batch, data.target)
+                
+                # TODO: hardcoded from ONNX save script output...
+                x=data.x
+                x=torch.zeros((70,78),dtype=torch.float32)
+                x[:data.x.size(0),:data.x.size(1)]=data.x
+                
+                maxVertex=torch.max(data.edge_index)+1
+                maxVertexPossible=70
+                ###no self connections if original graph has self connections
+                if maxVertex<maxVertexPossible:
+                    edge_index=torch.randint(low=64, high=70, size=(2,150), dtype=torch.int64)
+                    edge_index[:data.edge_index.size(0),:data.edge_index.size(1)]=data.edge_index
+                else:
+                    edge_index=data.edge_index
+                    print("no edge padding, edge_index.shape={}, x.shape={}".format(edge_index.shape, data.x.shape))
+                
+                #edge_index=data.edge_index
+
+                batch=torch.ones((70),dtype=torch.int64)
+                batch[:data.batch.size(0)]=data.batch
+                #batch=torch.ones(1,dtype=torch.int64)
+                #batch=torch.zeros(1,dtype=torch.int64)  
+                target=torch.zeros((2,958),dtype=torch.float32)
+                target[:data.target.size(0), :data.target.size(1)]=data.target
+
+                outputs = self.model(
+                    x=x,
+                    edge_index=edge_index,
+                    batch=batch,
+                    target=target
+                )
+                print(outputs[0][0])
+                total_labels = torch.cat((total_labels, data.y.view(-1,1).cpu()), 0)
+                total_preds = torch.cat((total_preds, torch.tensor(outputs[0][0])), 0)
+                
+        total_labels=total_labels.numpy().flatten()
+        total_preds=total_preds.numpy().flatten()
+        print(total_preds)
+        # ------------------------------------------------------
+        # [Req] Save raw predictions in dataframe
+        # ------------------------------------------------------
+        frm.store_predictions_df(
+            self.params,
+            y_true=total_labels, y_pred=total_preds, stage="test",
+            outdir=self.params["infer_outdir"]
+        )
+
+        # ------------------------------------------------------
+        # [Req] Compute performance scores
+        # ------------------------------------------------------
+        test_scores = frm.compute_performace_scores(
+            self.params,
+            y_true=total_labels, y_pred=total_preds, stage="test",
+            outdir=self.params["infer_outdir"], metrics=metrics_list
+        )
+
+        return test_scores
